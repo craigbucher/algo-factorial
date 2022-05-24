54c43881017124b8f4aec82807c6b30aea69e86e
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 16:12:53 -0500

    initial js code

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,10 @@
 exports.factorial = function(num) {
+    let result = 1;	
+    // factorial 0! = 1	
+    if (num === 0){	
+      return 1;	
+    } else {
+    // test case of factorial(45) fails without recursion
+        return num * exports.factorial(num - 1);
+    }
 };
