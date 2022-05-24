fb085f23271413b5ef1a5a79437de93e80aa21da
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 16:22:15 -0500

    initial python

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,11 @@
 def factorial(num):
-	# your code here
+	result = 1
+	if num == 0:
+		return 1
+	else:
+		while num >= 1:
+			result *= num
+			num -= 1
+	return result
+# with recurison:
+#	return 1 if (n == 1 or n == 0) else n * factorial(n - 1);
